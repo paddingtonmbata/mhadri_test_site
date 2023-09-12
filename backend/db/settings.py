d071a1ce4af55d76bc053c9b92e0ea1b298aeca9
Kuda Mbata <kudambata2005@gmail.com>
2023-09-13 01:29:08 +0200

    database setup

--- backend/db/settings.py
+++ backend/db/settings.py
@@ -17,13 +17,7 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 #BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# Use SQLite locally
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+
 
 # Use PostgreSQL on Heroku
 if os.environ.get('DATABASE_URL'):
@@ -105,10 +99,11 @@ WSGI_APPLICATION = 'db.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# Use SQLite locally
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
